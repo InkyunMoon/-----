e690ae9812bee6a486ff27b7edc255cc2f0aa5cc
InkyunMoon <ik.moon23@gmail.com>
2021-03-24 11:20:23 +0900

    0324오전 전처리

--- 01. EDA.py
+++ 01. EDA.py
@@ -12,7 +12,6 @@ pd.set_option('display.max_columns', 50)
 data_path = os.path.join(os.getcwd(), 'SCALE불량.csv')
 raw_data = pd.read_csv(data_path, encoding='CP949')
 
-data = raw_data.copy()
 # df 형태 확인
 raw_data.shape # 720개의 데이터, 21개의 피쳐 확인
 
@@ -24,6 +23,8 @@ raw_data['WORK_GR'] = raw_data['WORK_GR'].astype('object')
 raw_data['HSB'] = raw_data['HSB'].astype('object')
 raw_data['SCALE'] = raw_data['SCALE'].astype('object')
 
+# raw_data.drop('ROLLING_DATE', axis=1, inplace = True) # 날짜 제거
+
 ############################## 1. 먼저 범주형 데이터를 살펴본다.##############################
 raw_data.select_dtypes(include='object').info() # PLATE_NO ROLLING_DATE SCALE SPEC STEEL_KIND FUR_NO HSB WORK_GR 에 대해서 각각 조사
 
@@ -32,7 +33,8 @@ for col in raw_data.select_dtypes(include='object'):
     print(raw_data[col].value_counts()) # ROLLING_DATE -> 너무 세세한 시간별로 나뉨, 시간대별로 범주화하기 -> 아직 완료X
     
 # plate_no
-sum(raw_data['PLATE_NO'].value_counts().values == 1) # 데이터별로 유니크한 값을 가진다.
+sum(raw_data['PLATE_NO'].value_counts().values == 1) # 데이터별로 유니크한 값을 가지므로 제거
+raw_data.drop('PLATE_NO', axis=1, inplace = True)
 
 # SCALE
 raw_data['SCALE'] = np.where(raw_data['SCALE'] == '양품', 0, 1) # 불량:1 양품:0으로 변환
@@ -40,8 +42,12 @@ print('불량품이 전체의' ,sum(raw_data['SCALE'] == 1)/(raw_data['SCALE']).
 sns.countplot(raw_data['SCALE'])
 
 # SPEC --> 보류
-raw_data['SPEC'].value_counts()[:35]
-raw_data['SPEC']
+raw_data['SPEC'].value_counts().plot(kind='bar')
+len(raw_data['SPEC'].unique()) # 66개 종류의 SPEC
+len(raw_data["SPEC"].value_counts()[raw_data["SPEC"].value_counts() == 1]) # 유니크한 스펙의 개수 == 12
+idx_more4 = raw_data["SPEC"].value_counts()[raw_data["SPEC"].value_counts() >= 4].index
+spec4_data = []
+#가설 검정을 통해 삭제할지말지 결정하자... 전에 선형결합으로 나타낼 수 있다는 정보는 어디서 얻었는지???
 
 # STEEL_KIND
 raw_data['STEEL_KIND'].value_counts() # C0 강재가 대다수(69.86%)를 차지함을 알 수 있다.
@@ -95,9 +101,17 @@ sns.heatmap(corr,
 
 # FUR_SZ_TEMP와 FUR_EXTREMP의 상관관계가 1. FUR_EXTEMP 열 드랍(가열로 균열대 온도와 가열로에서 나왔을때의 온도가 당연히 비례할 것)
 raw_data.drop('FUR_EXTEMP', axis=1, inplace = True)
+data_cont.drop('FUR_EXTEMP', axis=1, inplace = True)
+
 # FUR_SZ_TEMP와 FUR_HZ_TEMP -> 0.77
 sns.pairplot(data_cont)
-##### -> 공선성문제, 모델링에서 해결하도록 한다.
+##### -> 공선성문제, 회귀 모델에서 해결하도록 한다.
+# 산점도를 통해 ROLLING_DESCALING과 SCALE간에 일정한 패턴이 존재함을 확인
+# ROLLING_DESCALING
+sns.catplot(x='ROLLING_DESCALING', y='SCALE', data=raw_data) # DESCALING이 홀수인 경우, 스케일링이 100%발생한다.
+# 산점도 -> FUR_SZ_TEMP가 1175를 초과하면 반드시 스케일링 발생
+# 산점도 -> ROLLING_TEMP_T5가 1000을 초과하면 반드시 스케일링 발생
+# 산점도 -> FUR_HZ_TEMP가 1188를 초과하면 스케일링 발생빈도 급격히 증가
 
 # ROLLING_TEMP_T5
 sns.catplot(y = 'ROLLING_TEMP_T5', data = raw_data, height = 4, kind = 'box', palette = 'muted') # 0이 6개 있다.
@@ -115,7 +129,7 @@ for col in data_cont.columns:
 # PT_THK
 # PT_WDTH
 # FUR_HZ_TIME
-# FUR_SZ_TIME 이 네가지, 1.5IQR을 벗어나는 데이터 다수 존재. 4개 피쳐에서 동시에 2개 이상의 피쳐가 fence를 벗어나는 경우, 삭제하도록 하자.
+# FUR_SZ_TIME 이 네가지, 1.5*IQR을 벗어나는 데이터 다수 존재. 4개 피쳐에서 동시에 2개 이상의 피쳐가 fence를 벗어나는 경우, 삭제하도록 하자.
 
 # 데이터프레임, 살펴볼 피쳐, 개수를 입력하면 이상치의 인덱스를 리턴하는 함수를 만든다.
 def detect_outliers(df, n, features):
@@ -138,17 +152,15 @@ def detect_outliers(df, n, features):
     # 탐지된 이상치가 n개 이상인 샘플에 대한 인덱스 리스트를 만든다.
     return multiple_outliers
 
-detect_outliers(raw_data, 2, ['PT_THK','PT_WDTH','FUR_HZ_TIME','FUR_SZ_TIME']) # 두개 이상 피쳐에서 아웃라이어가 있는 인덱스 제거
+# 두개 이상 피쳐에서 아웃라이어가 있는 인덱스 제거?
+idx_selected = detect_outliers(raw_data, 2, ['PT_THK','PT_WDTH','FUR_HZ_TIME','FUR_SZ_TIME'])
+# 1개 이상 피쳐로 선택하면 114개의 데이터가 선택되므로 보수적 조건(2개) 설정
+raw_data.drop(idx_selected, axis=0, inplace = True)
+# -> 선택된 데이터 모두 스케일이 발생하지 않음. 그러나 평균 두 피쳐에서 평균으로부터 크게 벗어나 있으므로 제거한다.
 
-
-
-
-
-
-
-
-
-
+# 논문 참고, 파생변수 생성
+raw_data['temp_loss'] = raw_data['FUR_SZ_TIME'] - raw_data['ROLLING_TEMP_T5']
+# -> loss가 크면 스케일링 발생빈도 증가
 
 
 
